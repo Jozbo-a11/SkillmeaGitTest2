afcd74bcb5d154ab9c6b5cb1618acd5dc72f13a5
Jozbo-a11 <jozef.boko@gmail.com>
2025-05-29 17:08:21 +0200

    first comit

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,31 @@
 public class Main {
     public static void main(String[] args) {
 
-        String pozdrav = "Ahoj svet";
+        /*String pozdrav = "Ahoj svet";
 
 
         System.out.println(pozdrav);
         System.out.println(pozdrav);
         System.out.println(pozdrav);
-        System.out.println(pozdrav);
+        System.out.println(pozdrav);*/
+
+        /*int prveCislo = 10;
+        int druheCislo = 5;
+
+        System.out.println(prveCislo + druheCislo);
+        System.out.println(prveCislo - druheCislo);
+        System.out.println(prveCislo * druheCislo);
+        System.out.println(prveCislo / druheCislo);
+
+        String pozdrav2 = "Ahoj";
+        String meno = "Jozef";
+
+        System.out.println(pozdrav2 + " " + meno);*/
+
+
+
+
+
 
     }
 }
