0cc31aeda4d80a26b86f57c22823d1839a5ecd76
Jozbo-a11 <jozef.boko@gmail.com>
2025-05-29 17:37:05 +0200

    first comit

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
@@ -17,10 +19,37 @@ public class Main {
         System.out.println(prveCislo * druheCislo);
         System.out.println(prveCislo / druheCislo);
 
+
+
         String pozdrav2 = "Ahoj";
         String meno = "Jozef";
 
         System.out.println(pozdrav2 + " " + meno);*/
+
+        //Uloha: Uxivatel napise svoje meno a program ho pozdravi
+        /*String pozdrav3 = "Ahoj, ";
+        Scanner mojScanner = new Scanner(System.in);
+        System.out.println("Zadaj svoje meno:");
+        String meno = mojScanner.nextLine();
+        System.out.println(pozdrav3 + meno);*/
+
+        //Kalkulacka
+        /*float prveCislo;
+        float druheCislo;
+        Scanner mojScanner = new Scanner(System.in);
+
+        System.out.println("Zadaj prve cislo:");
+        prveCislo = mojScanner.nextFloat();
+
+        System.out.println("Zadaj druhe cislo:");
+        druheCislo = mojScanner.nextFloat();
+
+        System.out.println(prveCislo + druheCislo);
+        System.out.println(prveCislo - druheCislo);
+        System.out.println(prveCislo * druheCislo);
+        System.out.println(prveCislo / druheCislo);*/
+
+
 
 
 
